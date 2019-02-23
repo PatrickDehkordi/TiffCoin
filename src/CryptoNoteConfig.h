47a2d86e4cdc7eb176b2c26c16a5386e587f78f3
Patrick Dehkordi <patrick.dehkordi@gmail.com>
2019-02-23 14:56:26 -0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -108,7 +108,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 const std::initializer_list<const char*> SEED_NODES = {
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
- "70.175.33.156:8080"
+ "70.175.33.156:8080",
+ "35.184.24.98:8080" 
 };
 
 struct CheckpointData {
